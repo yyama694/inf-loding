ccece80a4d54bd9fc7e387090dfe2c91fd3aa85c
yyama <dummy@ne.jp>
2018-11-23 14:59:15 +0900

    無限スクロールいったん完成

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,58 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div>
+<table>
+  <tr v-for="(item, $index) in list" :key="$index">
+    <td><a :href="item.url">{{item.title}}</a></td>
+  </tr>
+</table>
+<infinite-loading @infinite="infiniteHandler" :distance="10"></infinite-loading>
+</div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import InfiniteLoading from 'vue-infinite-loading'
+import axios from 'axios'
+import Vue from "vue";
+Vue.use(InfiniteLoading, {
+  system: {
+    throttleLimit: 500,
+    /* other settings need to configure */
+  },
+});
+const api = 'https://qiita.com/api/v2/items'
 export default {
   name: "app",
   components: {
-    HelloWorld
+    InfiniteLoading
+  },
+  data() {
+    return {
+      page: 1,
+      list: [],
+    }
+  },
+  methods: {
+    infiniteHandler($state) {
+      axios.get(api, {
+        params: {
+          page: this.page,
+          per_page: 20
+        },
+        headers: {
+          'Authorization' : 'Bearer ' + '56b474b63a62df4112ff7d341c8dfc8a24960f50'
+        }
+      }).then(({ data }) => {
+        if (data.length) {
+          this.page += 1
+          this.list.push(...data)
+          $state.loaded()
+        } else {
+          $state.complete()
+        }
+      }).catch((err) => {
+          $state.complete()
+      })
+    }
   }
-};
-</script>
-
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
-</style>
+</script>
